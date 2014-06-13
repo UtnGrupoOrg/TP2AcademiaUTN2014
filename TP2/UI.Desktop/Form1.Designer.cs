837c4f52dc3edb54981acc6a76add194ddfd57ec
fbr1 <fbr1.fake@geemail.com>
2014-06-13 15:29:55 -0300

    add diseño formulario de login

--- TP2/UI.Desktop/Form1.Designer.cs
+++ TP2/UI.Desktop/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UI.Desktop
 {
-    partial class Form1
+    partial class formLogin
     {
         /// <summary>
         /// Variable del diseñador requerida.
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblBienvenida = new System.Windows.Forms.Label();
+            this.lblUsu = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.txtUsu = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.lnkOlvidaPass = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // lblBienvenida
+            // 
+            this.lblBienvenida.AutoSize = true;
+            this.lblBienvenida.Location = new System.Drawing.Point(81, 32);
+            this.lblBienvenida.Name = "lblBienvenida";
+            this.lblBienvenida.Size = new System.Drawing.Size(202, 26);
+            this.lblBienvenida.TabIndex = 0;
+            this.lblBienvenida.Text = "¡Bienvenido al Sistema!\r\nPor favor digite su información de Ingreso";
+            this.lblBienvenida.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUsu
+            // 
+            this.lblUsu.AutoSize = true;
+            this.lblUsu.Location = new System.Drawing.Point(30, 80);
+            this.lblUsu.Name = "lblUsu";
+            this.lblUsu.Size = new System.Drawing.Size(101, 13);
+            this.lblUsu.TabIndex = 1;
+            this.lblUsu.Text = "Nombre de Usuario:";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(67, 107);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(64, 13);
+            this.lblPass.TabIndex = 2;
+            this.lblPass.Text = "Contraseña:";
+            // 
+            // txtUsu
+            // 
+            this.txtUsu.Location = new System.Drawing.Point(137, 77);
+            this.txtUsu.Name = "txtUsu";
+            this.txtUsu.Size = new System.Drawing.Size(194, 20);
+            this.txtUsu.TabIndex = 3;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(137, 104);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(194, 20);
+            this.txtPass.TabIndex = 4;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(256, 130);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // lnkOlvidaPass
+            // 
+            this.lnkOlvidaPass.AutoSize = true;
+            this.lnkOlvidaPass.Location = new System.Drawing.Point(10, 158);
+            this.lnkOlvidaPass.Name = "lnkOlvidaPass";
+            this.lnkOlvidaPass.Size = new System.Drawing.Size(106, 13);
+            this.lnkOlvidaPass.TabIndex = 6;
+            this.lnkOlvidaPass.TabStop = true;
+            this.lnkOlvidaPass.Text = "Olvidé mi contraseña";
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(367, 181);
+            this.Controls.Add(this.lnkOlvidaPass);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsu);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblUsu);
+            this.Controls.Add(this.lblBienvenida);
+            this.Name = "formLogin";
+            this.Text = "formLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblBienvenida;
+        private System.Windows.Forms.Label lblUsu;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.TextBox txtUsu;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.LinkLabel lnkOlvidaPass;
     }
 }
 
