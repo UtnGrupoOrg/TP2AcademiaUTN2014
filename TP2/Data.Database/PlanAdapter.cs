4d3c651697b6c4233af48dc77409c1fd95f35724
Simonpedro <pedritos506@hotmail.com>
2014-07-16 01:33:16 -0300

    Revert "Revert "Finalizacion de creacion clases de BusinessLOgic""
    This reverts commit ec51ab8a01a5bc2fcd194f0bafb739198acd2d1f.

---
+++ TP2/Data.Database/PlanAdapter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data.Database
+{
+    public class PlanAdapter : Adapter
+    {
+
+        public List<Business.Entities.Plan> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Business.Entities.Plan GetOne(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Business.Entities.Plan plan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
