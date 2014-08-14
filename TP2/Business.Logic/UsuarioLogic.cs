dbfbe63a41285e5d05d88eeba54b3f3c701b0797
lauticlc <lauti.clc@gmail.com>
2014-08-14 09:23:29 -0300

    Revert "FIX moved password validation to business layer"
    This reverts commit 244fc31d24521c59e12613ae9cc9c1736a46ae46.

--- TP2/Business.Logic/UsuarioLogic.cs
+++ TP2/Business.Logic/UsuarioLogic.cs
@@ -52,14 +52,14 @@ namespace Business.Logic
         /// <summary>
         /// Valida si el usuario y la contraseña son correctos.
         /// </summary>
-        public bool identificarUsuario(string usu, string pass) // TODO validar por valores nulos  
+        public bool identificarUsuario(string usu, string pass) // TODO validar por valores nulos
         {
             List<Usuario> listUsuarios = this.GetAll();
             Usuario usuario = listUsuarios.Find(u => u.NombreUsuario == usu);
             bool estado = false;
             if (usuario != null)
             {
-                if(usuario.Clave == pass)
+                if(usuario.isPassword(pass))
                 {
                     return estado = true;
                 }
