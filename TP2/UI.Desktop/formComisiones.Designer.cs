8231b7f6ec222656ccaeb0e045a03dc96c175d4a
Simonpedro <pedritos506@hotmail.com>
2014-10-28 17:31:41 -0300

    Boludeces

--- TP2/UI.Desktop/formComisiones.Designer.cs
+++ TP2/UI.Desktop/formComisiones.Designer.cs
@@ -31,16 +31,16 @@
             this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
             this.tlPrincipal = new System.Windows.Forms.TableLayoutPanel();
             this.dgvComisiones = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descComison = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.anioEspecialidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descPlan = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btActualizar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
             this.tsbEditar = new System.Windows.Forms.ToolStripButton();
             this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descComison = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.anioEspecialidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descPlan = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.toolStripContainer1.ContentPanel.SuspendLayout();
             this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
             this.toolStripContainer1.SuspendLayout();
@@ -55,11 +55,11 @@
             // toolStripContainer1.ContentPanel
             // 
             this.toolStripContainer1.ContentPanel.Controls.Add(this.tlPrincipal);
-            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(868, 329);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(672, 308);
             this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
             this.toolStripContainer1.Name = "toolStripContainer1";
-            this.toolStripContainer1.Size = new System.Drawing.Size(868, 354);
+            this.toolStripContainer1.Size = new System.Drawing.Size(672, 333);
             this.toolStripContainer1.TabIndex = 0;
             this.toolStripContainer1.Text = "toolStripContainer1";
             // 
@@ -81,11 +81,12 @@
             this.tlPrincipal.RowCount = 2;
             this.tlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlPrincipal.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tlPrincipal.Size = new System.Drawing.Size(868, 329);
+            this.tlPrincipal.Size = new System.Drawing.Size(672, 308);
             this.tlPrincipal.TabIndex = 0;
             // 
             // dgvComisiones
             // 
+            this.dgvComisiones.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvComisiones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvComisiones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id,
@@ -95,14 +96,43 @@
             this.tlPrincipal.SetColumnSpan(this.dgvComisiones, 2);
             this.dgvComisiones.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvComisiones.Location = new System.Drawing.Point(3, 3);
+            this.dgvComisiones.MultiSelect = false;
             this.dgvComisiones.Name = "dgvComisiones";
-            this.dgvComisiones.Size = new System.Drawing.Size(862, 294);
+            this.dgvComisiones.Size = new System.Drawing.Size(666, 273);
             this.dgvComisiones.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "id_comision";
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // descComison
+            // 
+            this.descComison.DataPropertyName = "desc_comision";
+            this.descComison.HeaderText = "Descripcion";
+            this.descComison.Name = "descComison";
+            this.descComison.ReadOnly = true;
+            // 
+            // anioEspecialidad
+            // 
+            this.anioEspecialidad.DataPropertyName = "anio_especialidad";
+            this.anioEspecialidad.HeaderText = "Año Especialidad";
+            this.anioEspecialidad.Name = "anioEspecialidad";
+            this.anioEspecialidad.ReadOnly = true;
+            // 
+            // descPlan
+            // 
+            this.descPlan.DataPropertyName = "desc_plan";
+            this.descPlan.HeaderText = "Plan";
+            this.descPlan.Name = "descPlan";
+            this.descPlan.ReadOnly = true;
             // 
             // btActualizar
             // 
             this.btActualizar.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btActualizar.Location = new System.Drawing.Point(709, 303);
+            this.btActualizar.Location = new System.Drawing.Point(513, 282);
             this.btActualizar.Name = "btActualizar";
             this.btActualizar.Size = new System.Drawing.Size(75, 23);
             this.btActualizar.TabIndex = 1;
@@ -112,7 +142,7 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(790, 303);
+            this.btnSalir.Location = new System.Drawing.Point(594, 282);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(75, 23);
             this.btnSalir.TabIndex = 2;
@@ -163,41 +193,15 @@
             this.tsbEliminar.Text = "toolStripButton3";
             this.tsbEliminar.Click += new System.EventHandler(this.tsbEliminar_Click);
             // 
-            // id
-            // 
-            this.id.DataPropertyName = "id_comision";
-            this.id.HeaderText = "ID";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            // 
-            // descComison
-            // 
-            this.descComison.DataPropertyName = "desc_comision";
-            this.descComison.HeaderText = "Descripcion";
-            this.descComison.Name = "descComison";
-            this.descComison.ReadOnly = true;
-            // 
-            // anioEspecialidad
-            // 
-            this.anioEspecialidad.DataPropertyName = "anio_especialidad";
-            this.anioEspecialidad.HeaderText = "Año Especialidad";
-            this.anioEspecialidad.Name = "anioEspecialidad";
-            this.anioEspecialidad.ReadOnly = true;
-            // 
-            // descPlan
-            // 
-            this.descPlan.DataPropertyName = "desc_plan";
-            this.descPlan.HeaderText = "Plan";
-            this.descPlan.Name = "descPlan";
-            this.descPlan.ReadOnly = true;
-            // 
             // formComisiones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(868, 354);
+            this.ClientSize = new System.Drawing.Size(672, 333);
             this.Controls.Add(this.toolStripContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "formComisiones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Comisiones";
             this.Load += new System.EventHandler(this.formComisiones_Load);
             this.toolStripContainer1.ContentPanel.ResumeLayout(false);
