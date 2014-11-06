0ada11fb10aaf0937d1afc49e20b407c644e2880
Simonpedro <pedritos506@hotmail.com>
2014-11-06 10:24:14 -0300

    ADD Nombre de Cuenta en masterpage

--- TP2/UIWeb/Site.Master.cs
+++ TP2/UIWeb/Site.Master.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Security;
+using Business.Logic;
+
 
 namespace UIWeb
 {
@@ -12,7 +14,7 @@ namespace UIWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            this.lbtnCuenta.Text = (new UsuarioLogic().GetOne(Int32.Parse(HttpContext.Current.User.Identity.Name.ToString()))).NombreUsuario;
         }
 
         protected void btnSalir_Click(object sender, EventArgs e)
