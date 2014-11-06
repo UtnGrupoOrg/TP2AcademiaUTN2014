7f61556a3c72d8d972f0b42d081e3e32f9166f2a
lauticlc <lauti.clc@gmail.com>
2014-11-06 10:17:49 -0300

    ADD ABM Especialidades.aspx
    finish it

---
+++ TP2/UIWeb/admin/Especialidades.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="Especialidades" Language="C#" AutoEventWireup="true" CodeBehind="Especialidades.aspx.cs" MasterPageFile="~/Site.Master" Inherits="UIWeb.admin.Especialidades" %>
+
+<asp:Content ContentPlaceHolderID="PageContent" ID="ContentEspecialidades" runat="server">        
+
+    <asp:ScriptManager ID="ScriptManager" runat="server"></asp:ScriptManager>
+     <asp:UpdatePanel ID="UpdatePanel" runat="server">
+        <ContentTemplate>
+            <h1> Especialidades </h1>
+            <asp:Panel ID="gridConteniner"  CssClass="centered" runat="server">
+                <asp:Panel ID="gridPanel" runat="server">
+                    <asp:GridView ID="gridView" runat="server" AutoGenerateSelectButton="True" AutoGenerateColumns="False" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged" OnRowCreated="gridView_RowCreated1">
+                        <Columns>
+                            <asp:BoundField DataField="Descripcion" HeaderText="Descripcion" />
+                           
+                        </Columns>
+                        <SelectedRowStyle ForeColor="White" CssClass="rowselected" />
+                    </asp:GridView>
+
+                </asp:Panel>      
+
+                <asp:Panel ID="gridActionsPanel" runat="server">
+                    <asp:LinkButton ID="lbtnNuevo" CssClass="button formbutton" runat="server" Text="Button" OnClick="lbtnNuevo_Click" >Nuevo</asp:LinkButton>
+                    <asp:LinkButton ID="lbtnEditar" CssClass="button formbutton" runat="server" Text="Button" OnClick="lbtnEditar_Click" >Editar</asp:LinkButton>
+                    <asp:LinkButton ID="lbtnEliminar" CssClass="button formbutton" runat="server" Text="Button" OnClick="lbtnEliminar_Click" >Eliminar</asp:LinkButton>                    
+                </asp:Panel>
+            </asp:Panel>
+            <asp:Panel CssClass="centered" ID="formPanel" runat="server" Visible="false">
+                <asp:Label ID="lblDescripcion" runat="server" Text="Descripcion"></asp:Label>
+                <asp:TextBox ID="txtDescripcion" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvDescripcion" runat="server" ControlToValidate="txtDescripcion" CssClass="error" Display="Dynamic" ErrorMessage="RequiredFieldValidator">Debes completar este campo</asp:RequiredFieldValidator>
+                <br />
+                    <asp:Panel ID="formActionsPanel" runat="server">
+                    <asp:LinkButton ID="lbtnAceptar" CssClass="button formbutton" runat="server" Text="Button" CausesValidation="true" OnClick="lbtnAceptar_Click" >Aceptar</asp:LinkButton>
+                    <asp:LinkButton ID="lbtnCancelar" CssClass="button formbutton" runat="server" CausesValidation="false" Text="Button" OnClick="lbtnCancelar_Click">Cancelar</asp:LinkButton>
+                </asp:Panel>
+            </asp:Panel>
+        </ContentTemplate>
+     </asp:UpdatePanel>
+        
+    
+
+</asp:Content>
---
+++ TP2/UIWeb/admin/Especialidades.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Entities;
+using Business.Logic;
+
+namespace UIWeb.admin
+{
+    public partial class Especialidades : baseABM
+    {
+        EspecialidadLogic _logic;
+
+        private EspecialidadLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new EspecialidadLogic();
+                }
+                return _logic;
+            }
+        }
+
+        private Especialidad Entity { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                this.LoadGrid();
+            }
+        }
+        private void LoadGrid()
+        {
+            this.gridPanel.Visible = true;
+            this.gridActionsPanel.Visible = true;
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedId = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.txtDescripcion.Text = this.Entity.Descripcion;
+        }
+
+        private void LoadEntity(Especialidad especialidad)
+        {
+            especialidad.Descripcion = this.txtDescripcion.Text;
+        }
+        private void SaveEntity(Especialidad especialidad)
+        {
+            try
+            {
+                this.Logic.Save(especialidad);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        private void EnableForm(bool enable)
+        {
+            this.txtDescripcion.Enabled = enable;
+            this.lblDescripcion.Enabled = enable;
+
+        }
+
+        private void ClearForm()
+        {
+            this.txtDescripcion.Text = string.Empty;
+        }
+        protected void lbtnEditar_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.gridPanel.Visible = false;
+                this.gridActionsPanel.Visible = false;
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedId);
+                this.EnableForm(true);
+            }
+        }
+        protected void lbtnEliminar_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.gridPanel.Visible = false;
+                this.gridActionsPanel.Visible = false;
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedId);
+                this.lbtnAceptar.Text = "Eliminar";
+            }
+        }
+
+        protected void lbtnNuevo_Click(object sender, EventArgs e)
+        {
+            this.gridPanel.Visible = false;
+            this.gridActionsPanel.Visible = false;
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void lbtnAceptar_Click(object sender, EventArgs e)
+        {
+            Page.Validate();
+            if (Page.IsValid)
+            {
+                this.Entity = new Especialidad();
+                switch (this.FormMode)
+                {
+                    case FormModes.Alta:
+                        {
+                            this.LoadEntity(this.Entity);
+                            this.Entity.State = BusinessEntity.States.New;
+                            break;
+                        }
+                    case FormModes.Baja:
+                        {
+                            this.Entity.ID = this.SelectedId;
+                            this.Entity.State = BusinessEntity.States.Deleted;
+                            break;
+                        }
+                    case FormModes.Modificacion:
+                        {
+                            this.Entity.ID = this.SelectedId;
+                            this.Entity.State = BusinessEntity.States.Modified;
+                            this.LoadEntity(this.Entity);
+                            break;
+                        }
+                }
+                this.SaveEntity(this.Entity);
+                this.LoadGrid();
+                this.formPanel.Visible = false;
+
+
+            }
+        }
+        protected void lbtnCancelar_Click(object sender, EventArgs e)
+        {
+            this.LoadGrid();
+            this.formPanel.Visible = false;
+        }
+        protected void gridView_RowCreated1(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.Header)
+                e.Row.Cells[0].Style["display"] = "none";
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                e.Row.Cells[0].Style["display"] = "none";
+                e.Row.ToolTip = "Click to select row";
+                e.Row.Attributes["onclick"] = this.Page.ClientScript.GetPostBackClientHyperlink(this.gridView, "Select$" + e.Row.RowIndex);
+            }
+        }
+
+    }
+}
